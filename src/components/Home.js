cf218d3bee215f39edfa1de9ec1d0225a6f2aca1
Mansur Uyar <ymansuruyar@gmail.com>
2023-10-29 18:17:30 +0300

    homepage
    homepage css and layout done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import HomeBack from "../assets/homeBack.jpeg";
+import '../styles/home.css'
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className="mainPage" style={{ backgroundImage: `url(${HomeBack})` }}>
+      <div className="order">
+        <Link to="/menu">
+          <button>SİPARİŞ VER</button>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
